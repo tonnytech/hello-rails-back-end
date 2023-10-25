589b3bbf389f209a2388f5def215e345e958e8df
tonnytech <tonnytei4@gmail.com>
2023-10-25 12:47:38 +0300

    set cors header

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,6 +1,10 @@
 class GreetingsController < ApplicationController
+  
   def random
     random_greeting = Message.order('RANDOM()').first
+
+    response.headers['Access-Control-Allow-Origin'] = '*' # Set the CORS header
+
     render json: { message: random_greeting&.content }
   end
 end
