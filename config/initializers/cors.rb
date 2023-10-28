239ccc9d919d42060988934aca55b607c9084f63
tonnytech <tonnytei4@gmail.com>
2023-10-28 14:21:34 +0300

    remove cors restriction policy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3456"
-
-    resource "/greetings/random",
+    origins "*"
+    resource "*",
       headers: :any,
       methods: [:get]
   end
